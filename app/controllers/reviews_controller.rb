d6427f21f58c4de93af0ded186a90f57d1426dc8
Vincent Bedard-Legault <bedard_v@hotmail.fr>
2020-08-27 11:35:41 +0000

    review product feature completed

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,13 +1,19 @@
 class ReviewsController < ApplicationController
+  before_filter :deny_access, :unless => :current_user
 
   def create
-    raise 'Hello'
-    # product = Product.find_by(id: params[:product_id])
-    # product.reviews.create({
-    #   rating: params[:review][:rating],
-    #   description: params[:review][:description],
-    #   user_id: 1
-    # })
+    product = Product.find_by(id: params[:product_id])
+    product.reviews.create({
+      rating: params[:review][:rating],
+      description: params[:review][:description],
+      user_id: session[:user_id]
+    })
+    redirect_to "/products/#{params[:product_id]}"
   end
 
+  def destroy
+    review = Review.find params[:id]
+    review.destroy
+    redirect_to "/products/#{params[:product_id]}", notice: 'Product deleted!'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
 
   resources :products, only: [:index, :show]
   resources :products do
-    resources :reviews, only: [:create]
+    resources :reviews, only: [:create, :destroy]
   end
   resources :categories, only: [:show]
 
